27f74721120d9d6e2738995b998aad8c7d3b8362
belikov_artem <dev.dreamvention@gmail.com>
2023-11-09 21:43:06 +0200

    INSTALL 2.2.0

---
+++ system/config/worldline.php
@@ -0,0 +1,132 @@
+<?php 
+$_['worldline_setting'] = array(
+	'version' => '1.0.0',
+	'account' => array(
+		'environment' => 'live',
+		'merchant_id' => array(),
+		'api_key' => array(),
+		'api_secret' => array(),
+		'api_endpoint' => array(
+			'live' => 'https://payment.direct.worldline-solutions.com',
+			'test' => 'https://payment.preprod.direct.worldline-solutions.com'
+		),
+		'webhook_key' => array(),
+		'webhook_secret' => array(),
+		'webhook_token' => '',
+		'cron_token' => ''
+	),
+	'advanced' => array(
+		'title' => array(),
+		'button_title' => array(),
+		'debug' => false,
+		'authorization_mode' => 'sale',
+		'group_cards' => true
+	),
+	'order_status' => array(
+		'created' => array(
+			'code' => 'created',
+			'name' => 'text_created_status',
+			'id' => 1
+		),
+		'cancelled' => array(
+			'code' => 'cancelled',
+			'name' => 'text_cancelled_status',
+			'id' => 7
+		),
+		'rejected' => array(
+			'code' => 'rejected',
+			'name' => 'text_rejected_status',
+			'id' => 8
+		),
+		'pending' =>  array(
+			'code' => 'pending',
+			'name' => 'text_pending_status',
+			'id' => 1
+		),
+		'captured' => array(
+			'code' => 'captured',
+			'name' => 'text_captured_status',
+			'id' => 5
+		),
+		'refunded' => array(
+			'code' => 'refunded',
+			'name' => 'text_refunded_status',
+			'id' => 11
+		)
+	),
+	'suggest' => array(
+		'company_name' => '',
+		'message' => ''
+	),
+	'environment' => array(
+		'live' => array(
+			'code' => 'live',
+			'name' => 'text_live',
+			'prefix' => '',
+			'api_endpoint' => 'https://payment.direct.worldline-solutions.com'
+		),
+		'test' => array(
+			'code' => 'test',
+			'name' => 'text_test',
+			'prefix' => '_test',
+			'api_endpoint' => 'https://payment.preprod.direct.worldline-solutions.com'
+		)
+	),
+	'authorization_mode' => array(
+		'pre_authorization' => array(
+			'code' => 'pre_authorization',
+			'name' => 'text_pre_authorization'
+		),
+		'final_authorization' => array(
+			'code' => 'final_authorization',
+			'name' => 'text_final_authorization'
+		),
+		'sale' => array(
+			'code' => 'sale',
+			'name' => 'text_sale'
+		)
+	),
+	'transaction_status' => array(
+		'created' => array(
+			'code' => 'created',
+			'name' => 'text_created'
+		),
+		'cancelled' => array(
+			'code' => 'cancelled',
+			'name' => 'text_cancelled'
+		),
+		'rejected' => array(
+			'code' => 'rejected',
+			'name' => 'text_rejected'
+		),
+		'rejected_capture' => array(
+			'code' => 'rejected_capture',
+			'name' => 'text_rejected_capture'
+		),
+		'pending_capture' => array(
+			'code' => 'pending_capture',
+			'name' => 'text_pending_capture'
+		),
+		'captured' => array(
+			'code' => 'captured',
+			'name' => 'text_captured'
+		),
+		'refunded' => array(
+			'code' => 'refunded',
+			'name' => 'text_refunded'
+		),
+		'authorization_requested' => array(
+			'code' => 'authorization_requested',
+			'name' => 'text_authorization_requested'
+		),
+		'capture_requested' => array(
+			'code' => 'capture_requested',
+			'name' => 'text_capture_requested'
+		),
+		'refund_requested' => array(
+			'code' => 'refund_requested',
+			'name' => 'text_refund_requested'
+		)
+	)
+);
+?>
